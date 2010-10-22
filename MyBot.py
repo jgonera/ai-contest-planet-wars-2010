96e17d710f975f960360fa590d9bce0ad62720c8
Juliusz Gonera <jgonera@gmail.com>
2010-10-22 20:25:15 +0200

    started rewriting defense algorithm

--- MyBot.py
+++ MyBot.py
@@ -12,45 +12,71 @@ log.setLevel(logging.DEBUG)
 
 
 class MyBot(BaseBot):
+	# TODO: catch TimeIsUp exception
 	first_turn = False
 	
 	def do_turn(self):
 		if self.first_turn:
 			self.first_turn = False
 			return
-			
-#		for target in self.universe.best_targets():
-#			log.debug(target.target_coefficient())
 		
-		#TODO: DRY!!! check if defense is correct
-		# defend
-		for target in self.universe.my_planets:
-			available_ships = 0
-			longest_distance = 0
-			best_sources = target.best_sources()
+		log.debug("DEFEND")
+		# TODO: sort planets (probably by growth)
+		for planet in self.universe.my_planets:
+			attacking_fleets = planet.attacking_fleets
+			safe_ship_count = planet.safe_ship_count()
 			
-			for source in best_sources:
-				attack_ship_count = source.attack_ship_count()
-				if attack_ship_count > 0:
-					available_ships += attack_ship_count
-					if source.distance(target) > longest_distance:
-						longest_distance = source.distance(target)
+			log.debug(attacking_fleets)
+			log.debug(planet.ship_count)
+			log.debug(safe_ship_count)
+			if len(attacking_fleets) != 0 and planet.ship_count < safe_ship_count:
+				defend = True
+				
+				# TODO: take into account already countered fleets in consecutive fleets when using defense_plan
+				#       (both in ship_counts of sources and in future fleet_safe_ship_counts)
+				defense_plan = [] # list of { planet, ship_count } to send for rescue
+				
+				for fleet in attacking_fleets:
+					fleet_safe_ship_count = planet.safe_ship_count(turns=fleet.turns_remaining)
+					
+					if planet.ship_count < fleet_safe_ship_count:
+						needed_ship_count = fleet_safe_ship_count - planet.ship_count
+						
+						best_sources = planet.best_sources(
+							min_ship_count=needed_ship_count,
+							max_distance=fleet.turns_remaining
+						)
+						
+						# don't try to defend if can't counter all the fleets
+						if len(best_sources) == 0:
+							log.debug("can't defend")
+							defend = False
+							break
+						
+						# TODO: use defense_plan, don't send until sure that can send
+						for source in best_sources:
+							source_sent_ships = min(source.ship_count - fleet_safe_ship_count, needed_ship_count)# TODO: make a method for that (min's 1st arg)?
+							source.send_fleet(planet, source_sent_ships)
+							needed_ship_count -= source_sent_ships
+							
+							if needed_ship_count <= 0:
+								break;
+							
+#							defense_plan.append({
+#								'source': best_sources[0],
+#								'ship_count': needed_ship_count
+#							})
+				
+				log.debug(defense_plan)
+				# if can't defend this planet, try next
+				if defend == False:
+					continue
+				
+#				for defense in defense_plan:
+#					defense['source'].send_fleet(planet, defense['ship_count'])
 		
-			target_in_future = target.in_future(longest_distance)
-			if target_in_future.owner == player.ME:
-				continue
-			if available_ships < target.safe_ship_count(longest_distance):
-				continue
-			
-			for source in best_sources:
-				target_in_future = target.in_future(longest_distance)
-				attack_ship_count = source.attack_ship_count()
-				if attack_ship_count > 0 and (not target_in_future.owner == player.ME):
-#					log.debug(source.id)
-#					log.debug(source.safe_ship_count())
-					source.send_fleet(target, min(attack_ship_count, target.safe_ship_count(longest_distance)))
 		
-		# attack
+		log.debug("ATTACK")
 		for target in self.universe.best_targets():
 			available_ships = 0
 			longest_distance = 0
@@ -130,20 +156,26 @@ class MyPlanet(Planet2):
 #		log.debug(value)
 		return value
 	
-	def best_sources(self, owner=player.ME):
-		planets = []
-		for planet in self.universe.find_planets(owner=owner):
-			if planet.id != self.id:
-				planets.append(planet)
+	def best_sources(self, owner=player.ME, max_distance=9999.0, min_ship_count=1):
+		planets = [ planet for planet in self.universe.find_planets(owner=owner) if (
+			planet.id != self.id and
+			planet.distance(self) <= max_distance
+		) ]
+		
+		# TODO: make it work for enemy too
+		if owner == player.ME:
+			available_planets = 0
+			for planet in planets:
+				available_planets += planet.ship_count - planet.safe_ship_count()
+				if available_planets >= min_ship_count:
+					break
+		
+			if available_planets < min_ship_count:
+				return []
 		
 		return sorted(
 			planets,
-			reverse=True,
-			key=lambda source: (
-				1.0 * (source.ship_count > source.safe_ship_count() or owner != player.ME) # lets assume enemy doesn't check this
-				* source.ship_count
-				/ float(source.distance(self))
-			)
+			key=lambda source: float(source.distance(self)) / float(source.ship_count + 1.0)
 		)
 	
 	def sources_coefficient(self, owner=player.ME):
@@ -186,7 +218,15 @@ class MyPlanet(Planet2):
 			value *= (float(len(self.universe.my_planets)) / float(len(self.universe.enemy_planets))) ** 2
 		
 		return value
-
+	
+	@property
+	def attacking_fleets(self):
+		fleets = self.universe.find_fleets(destination=self, owner=player.EVERYBODY - self.owner)
+		
+		return sorted(
+			fleets,
+			key=lambda fleet: fleet.turns_remaining
+		)
 
 class MyUniverse(Universe2):
 	
